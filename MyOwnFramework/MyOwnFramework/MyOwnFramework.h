61142a7d484c7960a879f52c921aa3f2d2a8c339
phani407 <phanibandaru23@gmail.com>
2020-05-15 12:08:50 +0530

    Finished my FrameWork

---
+++ MyOwnFramework/MyOwnFramework/MyOwnFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyOwnFramework.h
+//  MyOwnFramework
+//
+//  Created by RamiReddy on 15/05/20.
+//  Copyright © 2020 RamiReddy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyOwnFramework.
+FOUNDATION_EXPORT double MyOwnFrameworkVersionNumber;
+
+//! Project version string for MyOwnFramework.
+FOUNDATION_EXPORT const unsigned char MyOwnFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyOwnFramework/PublicHeader.h>
+
+
---
+++ MyOwnFramework/MyOwnFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  MyOwnFramework
+//
+//  Created by RamiReddy on 15/05/20.
+//  Copyright © 2020 RamiReddy. All rights reserved.
+//
+
+import Foundation
+
+
+public class Service{
+    private init(){}
+    
+    public static func myOwnMethodCall() ->String{
+        return "My Method Call Success through podfile !!!!"
+    }
+}
